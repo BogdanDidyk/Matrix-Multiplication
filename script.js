f1dbd6aa159bedc6a5f5b4957f62ba53dd86fc2a
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-04-15 23:15:14 +0300

    Added the function that generates random integer number

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function printMatrix(matrix) {
     matrix.forEach(row => console.log(row.join(" ")));
 }
+
+function getRandomInteger(rndMin = 0, rndMax = 9) {
+    return Math.floor(Math.random() * (rndMax - rndMin + 1)) + rndMin;
+}
