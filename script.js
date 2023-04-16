2cc587be8df7061e8e189a257c14841fc8fc7c21
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-04-16 23:49:01 +0300

    Added the function that generates random matrix

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function printMatrix(matrix) {
 function getRandomInteger(rndMin = 0, rndMax = 9) {
     return Math.floor(Math.random() * (rndMax - rndMin + 1)) + rndMin;
 }
+
+function getRandomMatrix(rows, cols, rndMin, rndMax) {
+    const matrix = [];
+
+    for (let i = 0; i < rows; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < cols; j++) {
+            matrix[i][j] = getRandomInteger(rndMin, rndMax);
+        }
+    }
+
+    return matrix;
+}
